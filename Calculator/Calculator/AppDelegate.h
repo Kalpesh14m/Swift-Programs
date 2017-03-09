56d0b3b27e1fa71fde7d51d082d2aaa0b602e312
Kalpesh14m <kalpesh.1.mali@gmail.com>
2017-03-08 20:40:02 -0800

    CalculatorDemo

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by gaurav on 08/03/17.
+//  Copyright © 2017 Kalpesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by gaurav on 08/03/17.
+//  Copyright © 2017 Kalpesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
